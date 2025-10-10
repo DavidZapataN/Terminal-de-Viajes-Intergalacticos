b2cceb11b771c27016ba9b26283407089fa91135
santiagogiraldo128 <santiago.giraldo@aldikase.com>
2025-10-10 11:43:02 -0500

    prueba de estilos en react con tailwind

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -1,6 +1,16 @@
+import { Card } from '../../../shared/components/Card'
+
 export const Login = () => {
   return (
-    <div>Login Page</div>
+    <div className="flex h-full w-full flex-col place-items-center justify-center gap-4">
+      <Card>
+        <div>
+          <h1 className="bg-gradient-to-r from-[#06ffa5] via-[#8b5cf6] to-[#06b6d4] bg-clip-text text-2xl font-bold text-transparent">
+            {' '}
+            Ejemplo{' '}
+          </h1>
+        </div>
+      </Card>
+    </div>
   )
 }
-
---
+++ src/shared/components/Card.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react'
+
+interface CardProps {
+  children: ReactNode
+}
+
+export const Card = ({ children }: CardProps) => {
+  return (
+    <div className="rounded-[15px] border border-transparent p-[1px] [background:linear-gradient(#111120,#111120)_padding-box,linear-gradient(45deg,#06ffa5,#8b5cf6,#06b6d4)_border-box]">
+      <div className="rounded-[15px] bg-[#111120] p-2">
+        {children}
+      </div>
+    </div>
+  )
+}
