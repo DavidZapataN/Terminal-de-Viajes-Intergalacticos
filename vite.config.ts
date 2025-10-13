ab1264d47447deedb4b254985ac87a1d0bb86960
Jose Miguel Pulgarin Agudelo <josemiguelp@solenium.co>
2025-10-12 23:11:44 -0500

    chore: update project configuration and dependencies
    - Added linting and formatting scripts to package.json.
    - Updated dependencies including React Query and Tailwind CSS.
    - Removed tsconfig.node.json and consolidated TypeScript configuration in tsconfig.json.
    - Enhanced tsconfig.json with stricter compiler options and included additional files.
    - Updated Vite configuration to integrate TanStack Router with auto code splitting and custom route generation.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { resolve } from 'node:path'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+  plugins: [
+    TanStackRouterVite({
+      autoCodeSplitting: true,
+      routesDirectory: './src/app/router/routes',
+      generatedRouteTree: './src/app/router/routeTree.gen.ts',
+      quoteStyle: 'single',
+    }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+})
